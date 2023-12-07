daccd653aa685d20be62db0142c5df81f25e6a42
xximxxhatedxx <64128637+xximxxhatedxx@users.noreply.github.com>
2023-12-07 08:58:03 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+$("#btn1").click(() => location.href='https://xximxxhatedxx.github.io/lab7/');
+$("#btn2").click(() => location.href='https://xximxxhatedxx.github.io/lab8/');
+$("#btn3").click(() => location.href='https://xximxxhatedxx.github.io/lab9/');
+$("#btn4").click(() => location.href='https://xximxxhatedxx.github.io/lab10/');
